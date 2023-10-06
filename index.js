3126aaba0ccfb99750aabe87633cdc8980b148fd
rasmus <4j534j@gmail.com>
2023-10-06 13:00:02 +0300

    Created API error handling

--- index.js
+++ index.js
@@ -26,13 +26,14 @@ const getWeatherDataPromise = (url) => {
 			let result = {
 				description: description, 
 				city: city,
-				temp: temp
+				temp: temp,
+				error: null
 			}
 			resolve(result)
 		})
 		.catch(error =>{
 			reject(error)
-		})
+			})
 	})
 }
 app.all("/", function (req,res){
@@ -49,63 +50,9 @@ app.all("/", function (req,res){
 	.then(data => {
 		res.render("index", data)
 	})
-})
-/*
-app.get("/", function (req,res){
-	let city = "Tartu"
-	let url = `https://api.openweathermap.org/data/2.5/weather?q=
-	${city}&appid=${key}`
-	getWeatherDataPromise(url)
-	.then(data => {
-		res.render("index", data)
-	})
-})
-app.post("/", function(req,res){
-	let city = req.body.cityname
-	url = `https://api.openweathermap.org/data/2.5/weather?q=
-	${city}&appid=${key}`
-	getWeatherDataPromise(url)
-	.then(data => {
-		res.render("index", data)
+	.catch(error => {
+		res.render("index", {error: "Problem with getting data, try again"})
 	})
 })
 
-let city = "Tartu"
-app.get("/", function(req, res){
-	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-	.then((response) =>  {
-		return response.json()
-	})
-
-	.then((data) => {
-		let description = data.weather[0].description
-		let city=data.name
-		let temp = Math.round(parseFloat(data.main.temp) -273.15)
-	res.render("index",{
-		description: description,
-		city: city, 
-		temp: temp
-	})
-	})
-})
-
-app.post("/", function(req, res){
-	let city = req.body.cityname
-	fetch(`https://api.openweathermap.org/data/2.5/weather?q=
-		${city}&appid=${key}`)
-	.then((response) => {
-		return response.json()
-	})
-	.then((data) =>{
-	let description = data.weather[0].description
-	let city=data.name
-	let temp = Math.round(parseFloat(data.main.temp) -273.15)
-	res.render("index",{
-		description: description,
-		city: city, 
-		temp: temp
-	})
-	})
-})
-*/
 app.listen(3000)
